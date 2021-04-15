d13416657ec4618650f7d7ea651522499220ab2e
Denis <den000.10@yandex.ru>
2021-04-15 22:02:38 +0300

    +slider

---
+++ js/script2.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1300,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../image/strela2.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../image/strela.svg"</button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {  
+                    dots: true,
+                    autoplay: true,
+                    infinite: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+  });
