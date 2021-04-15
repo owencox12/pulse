def10057093c7ecd9ce0baf6266fab803bcc84f8
owencox12 <82467133+owencox12@users.noreply.github.com>
2021-04-15 23:51:14 +0300

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1300,
-        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../image/strela2.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../image/strela.svg"</button>',
         responsive: [
