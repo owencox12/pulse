f6623b237150889a04efab7a9796fc0b5e6f2119
Denis <den000.10@yandex.ru>
2021-04-21 15:31:42 +0300

    finish part2

--- js/script2.js
+++ js/script2.js
@@ -36,4 +36,78 @@ $(document).ready(function(){
 
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
+
+      // modal window
+
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+      });
+      $('.button_mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+      });
+
+
+      function valideForms(form) {
+        $(form).validate({
+            rules : {
+                name: "required",
+                phone: "required",
+                email: {
+                   required: true,
+                   email: true
+                }
+            }, 
+            messages: {
+                name: "Пожалуйста введите свое имя",
+                phone: "Введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста введите свою почту",
+                  email: "Неправильно введен адресс почты"
+                }
+              }
+          });
+      };
+
+      valideForms('#consultation-form');
+      valideForms('#consultation form');
+      valideForms('#order form');
+
+
+      $('input[name=phone]').mask("+7 (999) 999-9999");
+
+      $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+      });
+      // Smooth scroll
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600){
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      });
+
+      $("a[href^=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+
   });
